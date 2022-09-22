8c155872ffac44adba649fe1c020836d2ffffd7a
Kasun pavithra <mandalawattegekasunpavithra@gmail.com>
2022-09-22 13:39:43 +0530

    reOrganized configuration files and added cors.

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,9 @@
+const config = require('config');
 const mongoose = require('mongoose');
 const { logger } = require('../utils/logger');
 
+
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/FTIQMS')
+    mongoose.connect(config.DB)
         .then(() => logger.info('Successfully conneted to the MongoDB..'));
 }
