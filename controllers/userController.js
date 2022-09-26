d0e7de9572762408e4c36a201ca8c5592efb3321
Kasun pavithra <mandalawattegekasunpavithra@gmail.com>
2022-09-27 03:13:43 +0530

    added firebase accessToken verification

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,20 @@
 const { User } = require('../models/User');
+const _ = require('lodash');
 
-module.exports = {}
+const getUserByNIC = async (req,res)=>{
+    if(!req.body.NIC) return res.sendStatus(400);
+
+    const result = await User.findOne({
+        NIC: req.body.NIC
+    });
+    if(!result) return res.json({success:false});
+    else{
+        return res.json({
+            success: true,
+            user: _.pick(result,["NIC", "firstName", "lastName", "mobile"])
+        });
+    }
+}
+
+
+module.exports = {getUserByNIC}
