7cdb97c2f2e05c80f8eeeb8a183cb7579e2e7f5a
Thivindu Paranayapa <paranayapathivindu@gmail.com>
2022-09-29 20:41:24 +0530

    View registered vehicles done.

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,9 +1,51 @@
 const _ = require('lodash');
 const { User } = require('../models/User');
+const { getVehicle } = require('./vehicleController');
 
 const updateFuelAllocation = (fuelType, fuelAllocation) => {
     
     return fuelAllocation;
 }
 
-module.exports = {updateFuelAllocation}
+const getAllRegisteredVehicles = async (req, res) => {
+    const customer = await User.findOne({
+        NIC: req.body.userNIC
+    }).select({
+        vehicles: 1
+    });
+
+    const vehicleDetailsList = [];
+
+    for (const vid of customer.vehicles) {
+        const vehicle = await getVehicle(vid.toString());
+        
+        if (vehicle.success)
+            vehicleDetailsList.push(vehicle.vehicle);
+        else
+            return res.json({success: false});
+      }
+
+    return res.json({
+        success: true,
+        allRegisteredVehicleDetails: vehicleDetailsList
+    });
+}
+
+const getRemainingFuel = async (req, res) => {
+    const customer = await User.findOne({
+        NIC: req.body.userNIC
+    }).select({
+        remainingFuel: 1
+    });
+
+    if(!customer)
+        return res.json({success: false});
+    else{
+        return res.json({
+            success: true,
+            fuel: customer.remainingFuel
+        });
+    }
+}
+
+module.exports = {updateFuelAllocation, getAllRegisteredVehicles, getRemainingFuel}
