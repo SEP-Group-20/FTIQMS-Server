934c393d7463e36e018c4cd8876dbd6945328df2
Kasun pavithra <mandalawattegekasunpavithra@gmail.com>
2022-10-03 20:37:58 +0530

    modified middlewares

--- midleware/verifyJWT.js
+++ midleware/verifyJWT.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
+//this piece of middleware gets the authorization header and validate it
 const verifyJWT = (req, res, next) => {
-    // console.log(req.headers)
+    // rejects request if there is no auth header
     const authHeader = req.headers.authorization || req.headers.Authorization;
     // console.log('dfdfdfd',authHeader);
     if (!authHeader?.startsWith('Bearer ')) return res.status(401).json("You're not Authenticated!");
@@ -9,8 +10,10 @@ const verifyJWT = (req, res, next) => {
     
     const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+        //reject request if invalid access token
         if (err) res.status(403).json({ auth: false, message: "Invalid token!" });
         else {
+            //goto next middleware if valid access token
             req.userID = decode.userInfo.id;
             req.role = decode.userInfo.role;
             next();
