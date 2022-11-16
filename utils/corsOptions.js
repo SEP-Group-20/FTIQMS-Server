de92914b872202697c78002e4de529c75f9b8db6
Kasun pavithra <mandalawattegekasunpavithra@gmail.com>
2022-11-16 17:11:06 +0530

    added log

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -6,6 +6,7 @@ const coresOptions = {
         //accept requests only from allowed origins
 
         const allowedOrigins = split(process.env.ALLOWED_ORIGINS, ",");
+        console.log(allowedOrigins);
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
